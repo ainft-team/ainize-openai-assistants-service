989862d687533fcf5b3b1681001dc6113cc15bba
minsulee2 <minsu@comcom.ai>
2024-03-22 15:05:10 +0900

    Separate blockchain cluster correctly

--- env.js
+++ env.js
@@ -1,9 +1,12 @@
 require('dotenv').config();
 
+const { BLOCKCHAIN_CLUSTER_NAMES } = require('./constants');
+
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 const AINIZE_PRIVATE_KEY = process.env.AINIZE_PRIVATE_KEY;
 const SLACK_WEBHOOK_TOKEN = process.env.SLACK_WEBHOOK_TOKEN;
 const SLACK_CHANNEL_NAME = process.env.SLACK_CHANNEL_NAME;
+const BLOCKCHAIN_CLUSTER = process.env.BLOCKCHAIN_CLUSTER
 
 if (!OPENAI_API_KEY && !AINIZE_PRIVATE_KEY) {
   throw new Error(`OPENAI_API_KEY(${OPENAI_API_KEY}) and AINIZE_PRIVATE_KEY(${AINIZE_PRIVATE_KEY}) are not defined.`);
@@ -11,6 +14,10 @@ if (!OPENAI_API_KEY && !AINIZE_PRIVATE_KEY) {
 
 if (!SLACK_WEBHOOK_TOKEN && !SLACK_CHANNEL_NAME) {
   throw new Error(`SLACK_WEBHOOK_TOKEN(${SLACK_WEBHOOK_TOKEN}) and SLACK_CHANNEL_NAME(${SLACK_CHANNEL_NAME}) are not defined.`);
+}
+
+if (![BLOCKCHAIN_CLUSTER_NAMES.MAINNET, BLOCKCHAIN_CLUSTER_NAMES.TESTNET].includes(BLOCKCHAIN_CLUSTER)) {
+  throw new Error(`given BLOCKCHAIN_CLUSTER must be either mainnet or testnet`);
 }
 
 let REST_MODE;
@@ -30,6 +37,7 @@ if (!process.env.VERBOSE) {
 module.exports = {
   OPENAI_API_KEY,
   AINIZE_PRIVATE_KEY,
+  BLOCKCHAIN_CLUSTER,
   REST_MODE,
   VERBOSE,
   SLACK_WEBHOOK_TOKEN,
