486ed21544e79062f3e607340b81259105bd11ae
minsulee2 <minsu@comcom.ai>
2024-03-28 15:28:22 +0900

    Add thread url test case

--- test/unit/openai_url_builder.test.js
+++ test/unit/openai_url_builder.test.js
@@ -5,10 +5,32 @@ const { OpenaiUrlBuilder } = require('../../data/openai_url_builder');
 
 describe("Openai_url_builder", () => {
   const OPENAI_ENDPOINT = 'https://api.openai.com/v1';
+  const MOCK_ASSISTANT_ID = 'assistantId';
+  const MOCK_THREAD_ID = 'threadId';
   
   describe("assistantBaseUrl()", () => {
-    it("generates assistant base url without ", () => {
+    it("generates assistant base url without path", () => {
       expect(OpenaiUrlBuilder.assistantBaseUrl({})).to.be.equal(`${OPENAI_ENDPOINT}/assistants`)
-    })
+    });
+  });
+
+  describe("assistantBaseWithAssistantIdUrl()", () => {
+    it("generates the url with assistant id path", () => {
+      expect(OpenaiUrlBuilder.assistantBaseWithAssistantIdUrl({ assistantId: MOCK_ASSISTANT_ID}))
+      .to.be.equal(`${OPENAI_ENDPOINT}/assistants/${MOCK_ASSISTANT_ID}`);
+    });
+  });
+
+  describe("threadBaseUrl()", () => {
+    it("generates thread base url without path", () => {
+      expect(OpenaiUrlBuilder.threadBaseUrl({})).to.be.equal(`${OPENAI_ENDPOINT}/threads`);
+    });
+  });
+
+  describe("threadBaseWithThreadIdUrl()", () => {
+    it("generates the url with thread id path", () => {
+      expect(OpenaiUrlBuilder.threadBaseWithThreadIdUrl({ threadId: MOCK_THREAD_ID }))
+      .to.be.equal(`${OPENAI_ENDPOINT}/threads/${MOCK_THREAD_ID}`);
+    });
   });
 });
